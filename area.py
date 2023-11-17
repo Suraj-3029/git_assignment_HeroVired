f64ffe0656fe6b6e48fe8705c6c072949519eb24
S-master-developer <suraj.mishra.saan@gmail.com>
2023-11-17 22:42:19 +0530

    scleton of area.py

---
+++ area.py
@@ -0,0 +1,10 @@
+import math
+
+class GeometryCalculator:    
+    def calculate_circle_area(self, radius):
+        return 
+    def calculate_rectangle_area(self, length, width):
+        return 
+
+if __name__ == "__main__":
+    calculator = GeometryCalculator()
